5b5240bf601a8aac9202a2b4f568383fae9408cc
Marcos D Lopes <marcos.dl@aluno.ifsc.edu.br>
2023-02-23 09:08:34 -0300

    alteracao

--- praticas01/src/visao/TelaCad.java
+++ praticas01/src/visao/TelaCad.java
@@ -5,10 +5,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class TelaCad extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -31,11 +41,68 @@ public class TelaCad extends JFrame {
 	 */
 	public TelaCad() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 802, 536);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new MigLayout("", "[grow][304.00][grow]", "[153.00,grow][78.00,grow][78.00,grow,center][66.00,grow]"));
+		
+		JLabel lblNewLabel = new JLabel("Cadastro de pessoa");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		contentPane.add(lblNewLabel, "cell 1 0,alignx center");
+		
+		JLabel lblCPF = new JLabel("CPF:");
+		contentPane.add(lblCPF, "cell 0 1,alignx right");
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, "cell 1 1,grow");
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1.setHorizontalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 254, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(40, Short.MAX_VALUE))
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(38)
+					.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(41, Short.MAX_VALUE))
+		);
+		panel_1.setLayout(gl_panel_1);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		contentPane.add(lblNome, "cell 0 2,alignx right");
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, "cell 1 2,alignx center,aligny center");
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, 258, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(36, Short.MAX_VALUE))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(43)
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(44, Short.MAX_VALUE))
+		);
+		panel.setLayout(gl_panel);
+		
+		JButton btnCad = new JButton("Cadastrar");
+		contentPane.add(btnCad, "cell 1 3,alignx center");
 	}
-
 }
